649334778c5783b3f6fd50553bb37a171d973227
Michael Hinte <michaelhinte@michaels-mbp-2.attlocal.net>
2019-09-13 11:35:24 -0500

    initializes server

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# class Beer(models.Model):
+#     name = models.CharField(max_length=100)
+#     abv = models.FloatField()
+#     ibu = models.IntegerField()
+#     style = models.TextField(max_length=100)
+#     taste_profile = 
+#     container_type = 
+#     restaurant = 
+
