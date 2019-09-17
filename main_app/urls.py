24aed67f0e11bf0459f14f0f07d70d56c27a17d7
Michael Hinte <michaelhinte@Michaels-MacBook-Pro-2.local>
2019-09-17 15:49:31 -0500

    adds 'tap' functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     path('home/my_restaurants/', views.my_restaurants, name='my_restaurants'),
     path('discover/', views.discover, name='discover'),
     path('beers/<int:beer_id>/', views.beer_detail, name='beer_detail'),
-    path('cooler/<int:beer_id>/add/<int:user_id>', views.cooler_add, name='cooler_add'),
-    path('cooler/<int:beer_id>/remove/<int:user_id>', views.cooler_remove, name='cooler_remove'),
+    path('cooler/<int:beer_id>/add/<int:user_id>/', views.cooler_add, name='cooler_add'),
+    path('cooler/<int:beer_id>/remove/<int:user_id>/', views.cooler_remove, name='cooler_remove'),
     path('restaurants/<int:restaurant_id>/', views.restaurant_detail, name='restaurant_detail'),
-    path('restaurant/<int:restaurant_id>/add/<int:user_id>,', views.restaurant_add, name='restaurant_add'),
-    path('restaurant/<int:restaurant_id>/remove/<int:user_id>', views.restaurant_remove, name="restaurant_remove"),
+    path('restaurant/<int:restaurant_id>/add/<int:user_id>/,', views.restaurant_add, name='restaurant_add'),
+    path('restaurant/<int:restaurant_id>/remove/<int:user_id>/', views.restaurant_remove, name="restaurant_remove"),
     path('discover/search/', views.search, name='search'),
-
+    path('beers/<int:beer_id>/tap/<int:restaurant_id>/', views.tap_to_rest, name="tap_to_rest"),
 ]
